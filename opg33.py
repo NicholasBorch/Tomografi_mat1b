0065da06a9b524dd2857dd7fb4870ee597626950
Peter <p.vesterenglarsen@gmail.com>
2024-04-26 15:33:47 +0200

    Ny kode virker

--- opg33.py
+++ opg33.py
@@ -1,14 +1,33 @@
-from sympy import *
-def intersect_cell(i, j, a, N, rho, phi):
+# from sympy import *
+import numpy as np
+from numpy import cos, sin, pi
+def intersect_cell_oneliner(i, j, a, N, rho, phi):
     return ((2*a*(i-1)/N-a < (rho-(2*a*(j-1)/N-a)*cos(phi))/sin(phi) <= 2*a*i/N-a) or (2*a*(i-1)/N-a < ((rho-(2*a*j/N-a)*cos(phi))/sin(phi)) <= 2*a*i/N-a))
 
-def intersect_cell_readable(i, j, a ,N, rho, phi):
-    lower_x = (rho-(2*a*(j-1)/N-a)*cos(phi))/sin(phi) 
-    upper_x = (rho-(2*a*j/N-a)*cos(phi))/sin(phi)
+def intersect_cell(i, j, a ,N, rho, phi):
+    ## For vertikale linjer:
+    ## Udregn y-værdier for l i intervalendepunkterne:
+    lower_x_vert = (rho-(2*a*(j-1)/N-a)*cos(phi))/sin(phi) 
+    upper_x_vert = (rho-(2*a*j/N-a)*cos(phi))/sin(phi)
 
-    lower_y = 2*a*(i-1)/N-a
-    upper_y = 2*a*i/N-a
-    return lower_y < lower_x <= upper_y or lower_y < upper_x <= upper_y
+    ## Udregn y-værdier for C_ij:
+    lower_y_vert = 2*a*(i-1)/N-a
+    upper_y_vert = 2*a*i/N-a
 
+    ## For horisontale linjer:
+    lower_x_hor = (rho-(2*a*(i-1)/N-a)*sin(phi))/cos(phi)
+    upper_x_hor = (rho-(2*a*i/N-a)*sin(phi))/cos(phi)
 
-print(intersect_cell_readable(1,1, 5, 5, 2, 0.25*pi))
+    lower_y_hor = 2*a*(j-1)/N-a
+    upper_y_hor = 2*a*j/N-a
+
+    ## Returnerer sand hvis l skærer mindst én af de vertikale sider af C_ij. Ellers, returnerer falsk:
+    return (lower_y_vert < lower_x_vert <= upper_y_vert or 
+            lower_y_vert < upper_x_vert <= upper_y_vert or
+            lower_y_hor < lower_x_hor <= upper_y_hor or
+            lower_y_hor < upper_x_hor <= upper_y_hor)
+            
+rho = -.8
+p = 0.35
+arr = np.array([[intersect_cell(6-i, j, 5, 5, rho, p*pi) for j in range(1, 6)] for i in range(1, 6)])
+print(arr)
