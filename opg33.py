fc12209a1460a4bc52a3fe68bf8804ec25548fca
Peter <p.vesterenglarsen@gmail.com>
2024-04-26 16:02:58 +0200

    MERE FÆRDIG KODE

--- opg33.py
+++ opg33.py
@@ -27,7 +27,63 @@ def intersect_cell(i, j, a ,N, rho, phi):
             lower_y_hor < lower_x_hor <= upper_y_hor or
             lower_y_hor < upper_x_hor <= upper_y_hor)
             
-rho = -.8
+# rho = -.8
+# p = 0.35
+# arr = np.array([[intersect_cell(6-i, j, 5, 5, rho, p*pi) for j in range(1, 6)] for i in range(1, 6)])
+# print(arr)
+
+def get_length(i,j,a,N,rho,phi):
+    ## Hvis l ikke skærer C_ij, returner None
+    if not intersect_cell(i,j,a,N,rho,phi):
+        return None
+    ## Ellers fortsætter vi:
+    
+    ## For vertikale linjer:
+    ## Udregn y-værdier for l i intervalendepunkterne:
+    lower_x_vert = (rho-(2*a*(j-1)/N-a)*cos(phi))/sin(phi) 
+    upper_x_vert = (rho-(2*a*j/N-a)*cos(phi))/sin(phi)
+
+    ## Udregn y-værdier for C_ij:
+    lower_y_vert = 2*a*(i-1)/N-a
+    upper_y_vert = 2*a*i/N-a
+
+    ## For horisontale linjer:
+    lower_x_hor = (rho-(2*a*(i-1)/N-a)*sin(phi))/cos(phi)
+    upper_x_hor = (rho-(2*a*i/N-a)*sin(phi))/cos(phi)
+
+    lower_y_hor = 2*a*(j-1)/N-a
+    upper_y_hor = 2*a*j/N-a
+
+    ## Laver liste til skæringspunkter
+    skæring = []
+
+    ## Undersøg, om y-værdien i venstre x-intervalendepunkt er indenfor C_ij:
+    if lower_y_vert < lower_x_vert <= upper_y_vert:
+        ## Hvis ja, tilføj skæringspunktet (2a(i-1)/N-a, lower_x_vert) til listen
+        skæring.append(((2*a*(j-1)/N-a),lower_x_vert))
+
+    ## Undersøg, om y-værdien i højre x-intervalendepunkt er indenfor C_ij:
+    if lower_y_vert < upper_x_vert <= upper_y_vert:
+        ## Hvis ja, tilføj skæringspunktet (2ai/N-a, upper_x_vert) til listen
+        skæring.append(((2*a*j/N-a),upper_x_vert))
+
+    ## Undersøg, om x-værdien i nederste y-intervalendepunkt er indenfor C_ij:
+    if lower_y_hor < lower_x_hor <= upper_y_hor:
+        ## Hvis ja, tilføj skæringspunktet (lower_x_hor, 2a(i-1)/N-a) til listen
+        skæring.append((lower_x_hor, 2*a*(i-1)/N-a))
+    
+    ## Undersøg, om x-værdien i øverste y-intervalendepunkt er indenfor C_ij:
+    if lower_y_hor < upper_x_hor <= upper_y_hor:
+        ## Hvis ja, tilføj skæringspunktet (upper_x_hor, 2ai/N-a) til listen
+        skæring.append((upper_x_hor, 2*a*i/N-a))
+    
+    ## Returner længden mellem skæringspunkterne (vi er sikret, at der kun findes 2)
+    p1, p2 = skæring
+    return np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2) ## Returnerer euklidisk distance mellem skæringspunkter
+
+
+rho = 1
 p = 0.35
-arr = np.array([[intersect_cell(6-i, j, 5, 5, rho, p*pi) for j in range(1, 6)] for i in range(1, 6)])
+arr = np.array([[get_length(6-i, j, 5, 5, rho, p*pi) for j in range(1, 6)] for i in range(1, 6)])
 print(arr)
+    
